c890220fbc73cf719ff20e21b068e0ff627ad521
Amit Raj <amit_raj_from.nsearch@tech.gov.sg>
2024-10-11 12:29:02 +0800

    update redis db

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import streamlit as st
 import redis
 
 # Connect to Redis
-redis_host = "localhost"  # Change this if you're using a remote Redis server
+redis_host = "clustercfg.app.tsce7z.apse1.cache.amazonaws.com"  # Change this if you're using a remote Redis server
 redis_port = 6379
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0, decode_responses=True)
 
 # Initialize the counter if not already present
 if not redis_client.exists('visitor_counter'):
