e9b1311643aa7198ffa02a80f09c8dd8eeb5fd58
Amit Raj <amit_raj_from.nsearch@tech.gov.sg>
2024-10-12 10:42:58 +0800

    add streamlit ui

--- app/main.py
+++ app/main.py
@@ -1,16 +1,13 @@
-# import streamlit as st
+import streamlit as st
 import redis
 
 # Connect to Redis
 redis_host = "dbone-tsce7z.serverless.apse1.cache.amazonaws.com" 
 redis_port = 6379
 
-
-
 r = redis.Redis(host= redis_host, port=6379, db=0, ssl=True, decode_responses=True)
 
 r.ping()
-
 
 if not r.exists('visitor_counter'):
     r.set('visitor_counter', 0)
@@ -20,6 +17,6 @@ visitor_count = r.incr('visitor_counter')
 print(visitor_count)
 
 # Display the message
-# st.title("Welcome to My App")
-#st.write(f"This is the {visitor_count} visitor.")
+st.title("Welcome to My App")
+st.write(f"This is the {visitor_count} visitor.")
 
